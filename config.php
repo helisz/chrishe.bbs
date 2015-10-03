30aa3a57b8ae3663393125fd6a1b782f4b587b1d
helisz <helisz101@gmail.com>
2015-10-03 02:29:40 -0700

    Initial setup

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '166.62.28.83',
+    'database' => 'chrishe_bbs',
+    'username' => 'helisz',
+    'password' => '8959865Hl',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://bbs.chrishe.me',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
